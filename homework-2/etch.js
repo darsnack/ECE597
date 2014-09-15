7c797e10461007eb106718d888d5fceefea81d19
Kyle Daruwalla <daruwalla.kyle@gmail.com>
2014-09-15 14:25:55 -0400

    Using crazy linux kung fu to control cursor

--- homework-2/etch.js
+++ homework-2/etch.js
@@ -17,12 +17,15 @@ var ROW_SIZE = 21;
 var COL_SIZE = 43;
 var draw = false;
 var refersh = true;
+var initInt = true;
 var grid = new Array(ROW_SIZE);
+var prevGrid = new Array(ROW_SIZE);
 var currentRow = (ROW_SIZE - 1) / 2,
 	currentCol = (COL_SIZE - 1) / 2;
 
 init();
 clearScreen();
+setupScreen();
 initInterrupts();
 setInterval(drawScreen, 10);
 
@@ -30,6 +33,7 @@ setInterval(drawScreen, 10);
 function init () {
 	for (var i = 0; i < ROW_SIZE; i++) {
 		grid[i] = new Array(COL_SIZE);
+		prevGrid[i] = new Array(COL_SIZE);
 	};
 }
 
@@ -49,6 +53,8 @@ function initInterrupts () {
 	bone.attachInterrupt(downButton, true, bone.FALLING, function (x) {processButton('down');});
 	bone.attachInterrupt(clearButton, true, bone.FALLING, function (x) {processButton('clear');});
 	bone.attachInterrupt(drawButton, true, bone.FALLING, function (x) {processButton('draw');});
+
+	initInt = false;
 }
 
 function clearScreen () {
@@ -61,30 +67,51 @@ function clearScreen () {
 	refersh = true;
 }
 
+function setupScreen () {
+	clear();
+
+	setCursor(0, 0);
+
+	for (var i = 0; i < COL_SIZE; i++) {
+		process.stdout.write('_');
+	};
+
+	process.stdout.write('\n');
+
+	for (var i = 0; i < ROW_SIZE; i++) {
+		process.stdout.write('|');
+		for (var j = 0; j < COL_SIZE; j++) {
+			if (i === currentRow && j === currentCol) {
+				process.stdout.write('+');
+			} else {
+				process.stdout.write(grid[i][j]);
+			}
+			prevGrid[i][j] = grid[i][j];
+		};
+		process.stdout.write('|\n');
+	};
+
+	for (var i = 0; i < COL_SIZE; i++) {
+		process.stdout.write('_');
+	};
+
+	refersh = false;
+}
+
 function drawScreen () {
 	if (refersh) {
-		clear();
-
-		for (var i = 0; i < COL_SIZE; i++) {
-			process.stdout.write('_');
-		};
-
-		process.stdout.write('\n');
-
 		for (var i = 0; i < ROW_SIZE; i++) {
-			process.stdout.write('|');
 			for (var j = 0; j < COL_SIZE; j++) {
 				if (i === currentRow && j === currentCol) {
+					setCursor(i, j);
+					grid[i][j] = '+';
 					process.stdout.write('+');
-				} else {
+				} else if (grid[i][j] !== prevGrid[i][j]) {
+					setCursor(i, j)
 					process.stdout.write(grid[i][j]);
 				}
+				prevGrid[i][j] = grid[i][j];
 			};
-			process.stdout.write('|\n');
-		};
-
-		for (var i = 0; i < COL_SIZE; i++) {
-			process.stdout.write('_');
 		};
 
 		refersh = false;
@@ -92,12 +119,15 @@ function drawScreen () {
 }
 
 function processButton (button) {
+	if (initInt) {
+		return;
+	}
+
 	if (draw) {
 		grid[currentRow][currentCol] = 'x';
 	}
 
 	if (button === 'left') {
-		console.log('left');
 		if (currentCol <= 0) {
 			currentCol = 0;
 		} else{
@@ -106,7 +136,6 @@ function processButton (button) {
 
 		refersh = true;
 	} else if (button === 'right') {
-		console.log('right');
 		if (currentCol >= COL_SIZE - 1) {
 			currentCol = COL_SIZE - 1;
 		} else{
@@ -115,7 +144,6 @@ function processButton (button) {
 
 		refersh = true;
 	} else if (button === 'up') {
-		console.log('up');
 		if (currentRow <= 0) {
 			currentRow = 0;
 		} else{
@@ -124,7 +152,6 @@ function processButton (button) {
 
 		refersh = true;
 	} else if (button === 'down') {
-		console.log('down');
 		if (currentRow >= ROW_SIZE - 1) {
 			currentRow = ROW_SIZE - 1;
 		} else{
@@ -142,4 +169,9 @@ function processButton (button) {
 // A handy clear screen function that works on Linux
 function clear () {
 	console.log('\033[2J');
+}
+
+// A handy set cursor function that works on Linux
+function setCursor (row, col) {
+	console.log("\033[" + row + ";" + col + "H");
 }
