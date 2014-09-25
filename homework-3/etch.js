fc0184b993c20af34372955b0f1e7fa451a7a24e
Kyle Daruwalla <daruwalla.kyle@gmail.com>
2014-09-25 14:22:13 -0400

    Using child process instead of shelljs

--- homework-3/etch.js
+++ homework-3/etch.js
@@ -2,7 +2,7 @@
 "use strict";
 
 var bone = require('bonescript'),
-	shell = require('shelljs'),
+	shell = require('child_process'),
 	async = require('async'),
 	i2c = require('i2c');
 
@@ -190,7 +190,7 @@ function setColor (row, column, color) {
 
 	async.waterfall([
 		function (callback) {
-			shell.exec('i2cget -y 1 ' + MATRIX_ADDR + ' 0x' + green.toString(16), {silent:true}, function (error, output) {
+			shell.exec('i2cget -y 1 ' + MATRIX_ADDR + ' 0x' + green.toString(16), function (error, output) {
 				if (error) {
 					callback("in i2cget for green row: " + error);
 				} else{
@@ -199,7 +199,7 @@ function setColor (row, column, color) {
 			});
 		},
 		function (greenOutput, callback) {
-			shell.exec('i2cget -y 1 ' + MATRIX_ADDR + ' 0x' + red.toString(16), {silent:true}, function (error, output) {
+			shell.exec('i2cget -y 1 ' + MATRIX_ADDR + ' 0x' + red.toString(16), function (error, output) {
 				if (error) {
 					callback("in i2cget for red row: " + error);
 				} else{
